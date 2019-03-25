48d307f6c9f65b6bd2d1fed5e8c7ba6fb1260ada
David Saylor <david.saylor.jr@gmail.com>
2019-03-25 07:21:50 -0800

    MG-22 Updating to allow MOODLE 3.5

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018120300;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018112800;        // Requires this Moodle version
+$plugin->requires  = 2018051700;        // Requires this Moodle version
 $plugin->component = 'block_profile_field_requirement';  // Full name of the plugin (used for diagnostics)
